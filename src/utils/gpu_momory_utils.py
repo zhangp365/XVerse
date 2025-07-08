5327e88a95a637b275edda12690dfc359c9629fd
chengaorong <chengaorong@bytedance.com>
2025-07-08 21:10:20 +0800

    use threshold_mem instead threshold

--- src/utils/gpu_momory_utils.py
+++ src/utils/gpu_momory_utils.py
@@ -2,7 +2,7 @@ import torch
 from diffusers import AutoencoderKL, FluxTransformer2DModel
 import time
 
-threshold = 0.55
+threshold_mem = 9 * 1024 * 1024 * 1024 #9G
 class ForwardHookManager:
     def __init__(self):
         self._registered_models = []
@@ -15,6 +15,7 @@ class ForwardHookManager:
         return total - reserved
 
     def _free_up_memory(self, required_mem, cache_model = None):
+        print("len(self._load_order):", len(self._load_order))
         sorted_items = sorted(self._load_order.items(), key=lambda x: x[1])
         for model, value in sorted_items:
             if self._origin_states[model]['in_cuda']:
@@ -24,10 +25,10 @@ class ForwardHookManager:
                     del self._load_order[model]
                 torch.cuda.empty_cache()
                 if cache_model == None:
-                    if self._get_available_memory() * threshold >= required_mem:
+                    if self._get_available_memory() - threshold_mem >= required_mem:
                         return True
                 else:
-                    if self._get_available_memory() >= required_mem * threshold:
+                    if self._get_available_memory() >= threshold_mem:
                         return True
         return False
     def model_to_cuda(self, model):
@@ -53,14 +54,14 @@ class ForwardHookManager:
             available_mem = self._get_available_memory()
             required_mem = self._origin_states[model]['cuda_memory']
             if origin_device.type != 'cuda':
-                if available_mem * threshold < required_mem:
+                if available_mem - threshold_mem < required_mem:
                     self._free_up_memory(required_mem)
                         # raise RuntimeError(f"Insufficient GPU memory. Required: {required_mem}, Available: {self._get_available_memory()}")
                 model.to('cuda')
                 if model not in self._load_order:
                     self._load_order[model] = 3
             else:
-                if available_mem < required_mem * threshold:
+                if available_mem < threshold_mem:
                     self._free_up_memory(required_mem, model)
                         # raise RuntimeError(f"Insufficient GPU memory. Required: {required_mem}, Available: {self._get_available_memory()}")
             self._load_order[model] += 1
@@ -96,14 +97,14 @@ class ForwardHookManager:
                         required_mem = self._origin_states[model]['cuda_memory']
                         origin_device = model.device if hasattr(model, 'device') else self._origin_states[model]['origin_device']
                         if origin_device.type != 'cuda':
-                            if available_mem * threshold < required_mem:
+                            if available_mem - threshold_mem < required_mem:
                                 self._free_up_memory(required_mem)
                             model.to('cuda')
                             
                             if model not in self._load_order:
                                 self._load_order[model] = 3
                         else:
-                            if self._get_available_memory() < required_mem * threshold:
+                            if self._get_available_memory() < threshold_mem:
                                 self._free_up_memory(required_mem, model)
                         self._origin_states[model]['in_cuda'] = True
                         self._load_order[model] += 1
